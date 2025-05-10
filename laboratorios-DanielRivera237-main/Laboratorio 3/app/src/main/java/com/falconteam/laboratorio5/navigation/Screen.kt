5ee96a7efcf38b6ab98d7b557c44f02384cd7b48
DanielRivera237 <00048722@uca.edu,sv>
2025-05-09 18:21:01 -0600

    Labo5 actualizacion

--- laboratorios-DanielRivera237-main/Laboratorio 3/app/src/main/java/com/falconteam/laboratorio5/navigation/Screen.kt
+++
@@ -1,1 +0,0 @@
-package com.falconteam.laboratorio5.navigationsealed class Screen (val route: String){    object Home: Screen("home")    object Sensor: Screen("sensor")    object Todo: Screen("todo")}
